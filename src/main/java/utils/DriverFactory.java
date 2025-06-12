8bcd41ec6f102c6834f4f8cc5605eafe909573c9
Vu PM <vunguyen@advn.vn>
2025-06-12 16:37:47 +0700

    Update CI

--- src/main/java/utils/DriverFactory.java
+++ src/main/java/utils/DriverFactory.java
@@ -2,13 +2,25 @@ package utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverFactory {
     public static WebDriver createDriver(String browser) {
         switch (browser.toLowerCase()) {
             case "chrome":
-                return new ChromeDriver();
+                ChromeOptions options = new ChromeOptions();
+                if (Boolean.parseBoolean(System.getProperty("headless", "false"))) {
+                    options.addArguments("--headless=new", "--no-sandbox", "--disable-gpu", "--window-size=1920,1080");
+                }
+                // Add extra chrome options if provided
+                String chromeOptions = System.getProperty("chrome.options");
+                if (chromeOptions != null) {
+                    for (String arg : chromeOptions.split(" ")) {
+                        options.addArguments(arg.trim());
+                    }
+                }
+                return new ChromeDriver(options);
             case "firefox":
                 return new FirefoxDriver();
             default:
