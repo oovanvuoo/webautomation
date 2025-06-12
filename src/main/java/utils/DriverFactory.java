8ee67aaaa3d3f0825e7271df1ad625182c239747
Vu PM <vunguyen@advn.vn>
2025-06-12 16:40:03 +0700

    Update CI

--- src/main/java/utils/DriverFactory.java
+++ src/main/java/utils/DriverFactory.java
@@ -4,25 +4,35 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class DriverFactory {
     public static WebDriver createDriver(String browser) {
         switch (browser.toLowerCase()) {
             case "chrome":
-                ChromeOptions options = new ChromeOptions();
+                ChromeOptions chromeOptions = new ChromeOptions();
                 if (Boolean.parseBoolean(System.getProperty("headless", "false"))) {
-                    options.addArguments("--headless=new", "--no-sandbox", "--disable-gpu", "--window-size=1920,1080");
+                    chromeOptions.addArguments("--headless=new", "--no-sandbox", "--disable-gpu", "--window-size=1920,1080");
                 }
-                // Add extra chrome options if provided
-                String chromeOptions = System.getProperty("chrome.options");
-                if (chromeOptions != null) {
-                    for (String arg : chromeOptions.split(" ")) {
-                        options.addArguments(arg.trim());
+                String extraChromeOptions = System.getProperty("chrome.options");
+                if (extraChromeOptions != null) {
+                    for (String arg : extraChromeOptions.split(" ")) {
+                        chromeOptions.addArguments(arg.trim());
                     }
                 }
-                return new ChromeDriver(options);
+                return new ChromeDriver(chromeOptions);
             case "firefox":
-                return new FirefoxDriver();
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                if (Boolean.parseBoolean(System.getProperty("headless", "false"))) {
+                    firefoxOptions.addArguments("--headless");
+                }
+                String extraFirefoxOptions = System.getProperty("firefox.options");
+                if (extraFirefoxOptions != null) {
+                    for (String arg : extraFirefoxOptions.split(" ")) {
+                        firefoxOptions.addArguments(arg.trim());
+                    }
+                }
+                return new FirefoxDriver(firefoxOptions);
             default:
                 throw new IllegalArgumentException("Unsupported browser: " + browser);
         }
